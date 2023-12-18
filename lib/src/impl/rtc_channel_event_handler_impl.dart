30c9eed134354e2b4f920cfb1002c68e75952004
Alex Palomin <alexpalomin21@gmail.com>
2023-12-18 13:30:03 -0700

    Test for unhandled methods

--- lib/src/impl/rtc_channel_event_handler_impl.dart
+++ lib/src/impl/rtc_channel_event_handler_impl.dart
@@ -2,15 +2,15 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:agora_rtc_engine/src/classes.dart';
-import 'enum_converter.dart';
 import 'package:agora_rtc_engine/src/impl/event_handler_json.dart';
 import 'package:agora_rtc_engine/src/rtc_channel_event_handler.dart';
+
+import 'enum_converter.dart';
 
 // ignore_for_file: public_member_api_docs
 
 extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
-  void process(String channelId, String methodName, dynamic data,
-      [Uint8List? buffer]) {
+  void process(String channelId, String methodName, dynamic data, [Uint8List? buffer]) {
     List<dynamic> newData;
     if (methodName.startsWith('on')) {
       methodName = methodName.substring(2);
@@ -31,23 +31,20 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         rejoinChannelSuccess?.call(newData[0], newData[1], newData[2]);
         break;
       case 'LeaveChannel':
-        leaveChannel
-            ?.call(RtcStats.fromJson(Map<String, dynamic>.from(newData[0])));
+        leaveChannel?.call(RtcStats.fromJson(Map<String, dynamic>.from(newData[0])));
         break;
       case 'ClientRoleChanged':
-        clientRoleChanged?.call(ClientRoleConverter.fromValue(newData[0]).e,
-            ClientRoleConverter.fromValue(newData[1]).e);
+        clientRoleChanged?.call(
+            ClientRoleConverter.fromValue(newData[0]).e, ClientRoleConverter.fromValue(newData[1]).e);
         break;
       case 'UserJoined':
         userJoined?.call(newData[0], newData[1]);
         break;
       case 'UserOffline':
-        userOffline?.call(
-            newData[0], UserOfflineReasonConverter.fromValue(newData[1]).e);
+        userOffline?.call(newData[0], UserOfflineReasonConverter.fromValue(newData[1]).e);
         break;
       case 'ConnectionStateChanged':
-        connectionStateChanged?.call(
-            ConnectionStateTypeConverter.fromValue(newData[0]).e,
+        connectionStateChanged?.call(ConnectionStateTypeConverter.fromValue(newData[0]).e,
             ConnectionChangedReasonConverter.fromValue(newData[1]).e);
         break;
       case 'ConnectionLost':
@@ -66,18 +63,12 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         videoSizeChanged?.call(newData[0], newData[1], newData[2], newData[3]);
         break;
       case 'RemoteVideoStateChanged':
-        remoteVideoStateChanged?.call(
-            newData[0],
-            VideoRemoteStateConverter.fromValue(newData[1]).e,
-            VideoRemoteStateReasonConverter.fromValue(newData[2]).e,
-            newData[3]);
+        remoteVideoStateChanged?.call(newData[0], VideoRemoteStateConverter.fromValue(newData[1]).e,
+            VideoRemoteStateReasonConverter.fromValue(newData[2]).e, newData[3]);
         break;
       case 'RemoteAudioStateChanged':
-        remoteAudioStateChanged?.call(
-            newData[0],
-            AudioRemoteStateConverter.fromValue(newData[1]).e,
-            AudioRemoteStateReasonConverter.fromValue(newData[2]).e,
-            newData[3]);
+        remoteAudioStateChanged?.call(newData[0], AudioRemoteStateConverter.fromValue(newData[1]).e,
+            AudioRemoteStateReasonConverter.fromValue(newData[2]).e, newData[3]);
         break;
       case 'LocalPublishFallbackToAudioOnly':
         localPublishFallbackToAudioOnly?.call(newData[0]);
@@ -86,22 +77,17 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         remoteSubscribeFallbackToAudioOnly?.call(newData[0], newData[1]);
         break;
       case 'RtcStats':
-        rtcStats
-            ?.call(RtcStats.fromJson(Map<String, dynamic>.from(newData[0])));
+        rtcStats?.call(RtcStats.fromJson(Map<String, dynamic>.from(newData[0])));
         break;
       case 'NetworkQuality':
-        networkQuality?.call(
-            newData[0],
-            NetworkQualityConverter.fromValue(newData[1]).e,
+        networkQuality?.call(newData[0], NetworkQualityConverter.fromValue(newData[1]).e,
             NetworkQualityConverter.fromValue(newData[2]).e);
         break;
       case 'RemoteVideoStats':
-        remoteVideoStats?.call(
-            RemoteVideoStats.fromJson(Map<String, dynamic>.from(newData[0])));
+        remoteVideoStats?.call(RemoteVideoStats.fromJson(Map<String, dynamic>.from(newData[0])));
         break;
       case 'RemoteAudioStats':
-        remoteAudioStats?.call(
-            RemoteAudioStats.fromJson(Map<String, dynamic>.from(newData[0])));
+        remoteAudioStats?.call(RemoteAudioStats.fromJson(Map<String, dynamic>.from(newData[0])));
         break;
       case 'RtmpStreamingStateChanged':
         rtmpStreamingStateChanged?.call(
@@ -114,8 +100,7 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         transcodingUpdated?.call();
         break;
       case 'StreamInjectedStatus':
-        streamInjectedStatus?.call(newData[0], newData[1],
-            InjectStreamStatusConverter.fromValue(newData[2]).e);
+        streamInjectedStatus?.call(newData[0], newData[1], InjectStreamStatusConverter.fromValue(newData[2]).e);
         break;
       case 'StreamMessage':
         // if (kIsWeb || (Platform.isWindows || Platform.isMacOS || Platform.isAndroid)) {
@@ -129,8 +114,8 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         streamMessage?.call(newData[0], newData[1], buffer);
         break;
       case 'StreamMessageError':
-        streamMessageError?.call(newData[0], newData[1],
-            ErrorCodeConverter.fromValue(newData[2]).e, newData[3], newData[4]);
+        streamMessageError?.call(
+            newData[0], newData[1], ErrorCodeConverter.fromValue(newData[2]).e, newData[3], newData[4]);
         break;
       case 'ChannelMediaRelayStateChanged':
         channelMediaRelayStateChanged?.call(
@@ -139,8 +124,7 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         );
         break;
       case 'ChannelMediaRelayEvent':
-        channelMediaRelayEvent
-            ?.call(ChannelMediaRelayEventConverter.fromValue(newData[0]).e);
+        channelMediaRelayEvent?.call(ChannelMediaRelayEventConverter.fromValue(newData[0]).e);
         break;
       case 'MetadataReceived':
         Metadata metadata;
@@ -157,34 +141,20 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         metadataReceived?.call(metadata);
         break;
       case 'AudioPublishStateChanged':
-        audioPublishStateChanged?.call(
-            channelId,
-            StreamPublishStateConverter.fromValue(newData[0]).e,
-            StreamPublishStateConverter.fromValue(newData[1]).e,
-            newData[2]);
+        audioPublishStateChanged?.call(channelId, StreamPublishStateConverter.fromValue(newData[0]).e,
+            StreamPublishStateConverter.fromValue(newData[1]).e, newData[2]);
         break;
       case 'VideoPublishStateChanged':
-        videoPublishStateChanged?.call(
-            channelId,
-            StreamPublishStateConverter.fromValue(newData[0]).e,
-            StreamPublishStateConverter.fromValue(newData[1]).e,
-            newData[2]);
+        videoPublishStateChanged?.call(channelId, StreamPublishStateConverter.fromValue(newData[0]).e,
+            StreamPublishStateConverter.fromValue(newData[1]).e, newData[2]);
         break;
       case 'AudioSubscribeStateChanged':
-        audioSubscribeStateChanged?.call(
-            channelId,
-            newData[0],
-            StreamSubscribeStateConverter.fromValue(newData[1]).e,
-            StreamSubscribeStateConverter.fromValue(newData[2]).e,
-            newData[3]);
+        audioSubscribeStateChanged?.call(channelId, newData[0], StreamSubscribeStateConverter.fromValue(newData[1]).e,
+            StreamSubscribeStateConverter.fromValue(newData[2]).e, newData[3]);
         break;
       case 'VideoSubscribeStateChanged':
-        videoSubscribeStateChanged?.call(
-            channelId,
-            newData[0],
-            StreamSubscribeStateConverter.fromValue(newData[1]).e,
-            StreamSubscribeStateConverter.fromValue(newData[2]).e,
-            newData[3]);
+        videoSubscribeStateChanged?.call(channelId, newData[0], StreamSubscribeStateConverter.fromValue(newData[1]).e,
+            StreamSubscribeStateConverter.fromValue(newData[2]).e, newData[3]);
         break;
       case 'RtmpStreamingEvent':
         rtmpStreamingEvent?.call(
@@ -193,8 +163,8 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
         );
         break;
       case 'UserSuperResolutionEnabled':
-        userSuperResolutionEnabled?.call(newData[0], newData[1],
-            SuperResolutionStateReasonConverter.fromValue(newData[2]).e);
+        userSuperResolutionEnabled?.call(
+            newData[0], newData[1], SuperResolutionStateReasonConverter.fromValue(newData[2]).e);
         break;
       case 'ClientRoleChangeFailed':
         if (clientRoleChangeFailed != null) {
@@ -205,11 +175,11 @@ extension RtcChannelEventHandlerExt on RtcChannelEventHandler {
       case 'FirstRemoteVideoFrame':
         if (firstRemoteVideoFrame != null) {
           final json = OnFirstRemoteVideoFrameJson.fromJson(dataMap);
-          firstRemoteVideoFrame!(
-              json.uid, json.width, json.height, json.elapsed);
+          firstRemoteVideoFrame!(json.uid, json.width, json.height, json.elapsed);
         }
         break;
       default:
+        print('unhandled $methodName');
         // do nothing
         break;
     }
